ece8a02251eb0d56c3f8dc40e603725b84b84313
helpmehelpus <adrianortorres@gmail.com>
2022-09-14 20:37:34 +0930

    introduce addFloats to AddNumbers

--- src/AddNumbers.java
+++ src/AddNumbers.java
@@ -2,4 +2,7 @@ public class AddNumbers {
     public int addIntegers(int aNumber, int anotherNumber) {
         return aNumber + anotherNumber;
     }
+    public int addFloats(float aNumber, float anotherNumber) {
+        return aNumber + anotherNumber;
+    }
 }
