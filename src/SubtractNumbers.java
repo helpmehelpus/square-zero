b63a370d781987bdc9e579ddbce9f5ce0d7ec96e
helpmehelpus <adrianortorres@gmail.com>
2022-09-14 21:19:17 +0930

    introduce subractNumbers

---
+++ src/SubtractNumbers.java
@@ -0,0 +1,8 @@
+public class SubtractNumbers {
+    public int subtractIntegers(int aNumber, int anotherNumber) {
+        return aNumber - anotherNumber;
+    }
+    public int subtractFloats(float aNumber, float anotherNumber) {
+        return aNumber - anotherNumber;
+    }
+}
