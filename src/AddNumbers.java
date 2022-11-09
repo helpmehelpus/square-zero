d90e7f81c140592a9655f7891c2aed248aee06ae
helpmehelpus <adrianortorres@gmail.com>
2022-11-09 20:33:11 +1030

    change addFloats return type

--- src/AddNumbers.java
+++ src/AddNumbers.java
@@ -2,7 +2,7 @@ public class AddNumbers {
     public int addIntegers(int aNumber, int anotherNumber) {
         return aNumber + anotherNumber;
     }
-    public int addFloats(float aNumber, float anotherNumber) {
+    public float addFloats(float aNumber, float anotherNumber) {
         return aNumber + anotherNumber;
     }
 }
