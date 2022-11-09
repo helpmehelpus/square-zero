37c7fd3ca92e7e25c9de869ba2c3d3dbe8d668d9
helpmehelpus <adrianortorres@gmail.com>
2022-11-09 21:02:40 +1030

    revert rename

--- src/AddNumbers.java
+++ src/AddNumbers.java
@@ -1,8 +1,12 @@
-public class Calculator {
+public class AddNumbers {
     public int addIntegers(int aNumber, int anotherNumber) {
         return aNumber + anotherNumber;
     }
     public float addFloats(float aNumber, float anotherNumber) {
         return aNumber + anotherNumber;
+    }
+
+    public double addDoubles(float aDouble, float anotherDouble) {
+        return aDouble + anotherDouble;
     }
 }
